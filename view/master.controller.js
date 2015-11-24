04ca66cf66d5e797215fc7a2ada52ed6033f5f44
xiaowangren <wangren_0626@126.com>
2015-11-24 10:56:46 +0800

    ok

--- view/master.controller.js
+++ view/master.controller.js
@@ -8,16 +8,35 @@ sap.ui.controller("sap.ui.company.view.master", {
     //             $('#com_content_title').html(comp_name.comp_pnum);
                 var html = "";
                 var depArray = [
-                	{deptName:"总经理工作部",num:"57"},
-                	{deptName:"人力资源部",num:"7"},
-                	{deptName:"物资供应部",num:"9"},
-                	{deptName:"财务部",num:"12"},
-                	{deptName:"计划合同部",num:"6"},
-                	{deptName:"安质部",num:"6"}//,
-                // 	{deptName:"工程部",num:"6"},
-                // 	{deptName:"燃料部",num:"6"}
+                	{deptName:"总经理工作部",num:"1"},
+                	{deptName:"人力资源部",num:"2"},
+                	{deptName:"物资供应部",num:"3"},
+                	{deptName:"财务部",num:"4"},
+                // 	{deptName:"计划合同部",num:"5"},
+                // 	{deptName:"安质部",num:"6"},
+                // 	{deptName:"工程部",num:"7"},
+                // 	{deptName:"安质部",num:"8"},
+                // 	{deptName:"工程部",num:"9"},
+                // 	{deptName:"总经理工作部",num:"10"},
+                // 	{deptName:"人力资源部",num:"11"},
+                // 	{deptName:"物资供应部",num:"12"},
+                // 	{deptName:"财务部",num:"13"},
+                // 	{deptName:"计划合同部",num:"14"},
+                // 	{deptName:"安质部",num:"15"},
+                // 	{deptName:"工程部",num:"16"},
+                // 	{deptName:"安质部",num:"17"},
+                // 	{deptName:"工程部",num:"18"},
+                	{deptName:"燃料部",num:"99"}
                 ];
+                var num =20;
+                var len = depArray.length;
+                    if(len>5){
+                       num = num* len;
+                       var sty = num+"%";
+                       document.getElementById('strt_block_table').style.width=sty;
+                    }
                 for(var i=0;i<depArray.length;i++){
+                    
                     if(i==0){
                         html+='<div class="strt-part"><span class="line-h line-h-r"></span><div class="line-v"><span></span></div><div class="strt-name-div" id="com_content_table_'+i+'"></div></div>';
                     }else if(i==depArray.length-1){
